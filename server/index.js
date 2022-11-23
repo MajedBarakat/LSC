6f3818e64cae865f0874b1b0ddd230a913894e1c
MajedBarakat <majedbarakat@hotmail.com>
2022-11-23 12:50:05 +0100

    master

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const constants = require('./config/constants');
+
+//Connect to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
+const connectDB = async () =>{
+try {
+  await mongoose.connect(
+    constants.mongoURI,
+    {
+      keepAlive: true
+    },
+  );
+  
+} catch (error) {
+  console.log(error)
+}
+}
+connectDB()
+require('./models/UrlShorten');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,x-access-token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+require('./routes/urlshorten')(app);
+
+
+const PORT = 7000;
+app.listen(PORT, () => {
+  console.log(`Server started on port`, PORT);
+});
---
+++ server/routes/urlshorten.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const validUrl = require('valid-url');
+const UrlShorten = mongoose.model('UrlShorten');
+const constants = require('../config/constants');
+const shortCode = require('../middlewares/uniqueUrlCode');
+
+module.exports = app => {
+  app.get('/api/item/:code', async (req, res) => {
+    const urlCode = req.params.code;
+    const item = await UrlShorten.findOne({ urlCode: urlCode });
+    if (item) {
+      return res.redirect(item.originalUrl);
+    } else {
+      return res.redirect(constants.errorUrl);
+    }
+  });
+
+  app.get('/:code', async (req, res) => {
+    const urlCode = req.params.code;
+    const item = await UrlShorten.findOne({ urlCode: urlCode });
+    if (item) {
+      return res.redirect(item.originalUrl);
+    } else {
+      return res.redirect(constants.errorUrl);
+    }
+  });
+
+  app.post('/api/item', async (req, res) => {
+    const { shortBaseUrl, originalUrl } = req.body;
+    if (validUrl.isUri(shortBaseUrl)) {
+    } else {
+      return res.status(404).json('Invalid Base Url format');
+    }
+
+    const updatedAt = new Date();
+    const queryOptions = { originalUrl };
+    if (validUrl.isUri(originalUrl)) {
+      let urlData;
+      try {
+
+        if (!urlData) {
+          // Find the item is in the database
+          urlData = await UrlShorten.findOne(queryOptions).exec();
+        }
+        if (urlData) {
+          res.status(200).json(urlData);
+        } else {
+          const urlCode = shortCode.generate();
+          shortUrl = shortBaseUrl + '/' + urlCode;
+          const itemToBeSaved = { originalUrl, shortUrl, urlCode, updatedAt };
+
+          // Add the item to db
+          const item = new UrlShorten(itemToBeSaved);
+          await item.save();
+
+          res.status(200).json(itemToBeSaved);
+        }
+      } catch (err) {
+        res.status(401).json('Invalid User Id');
+      }
+    } else {
+      return res.status(401).json('Invalid Original Url.');
+    }
+  });
+};
